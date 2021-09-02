0f021bc80f6ea770e577215809dafa983e1a7265
shwnyao <shawn.swyao@gmail.com>
2021-09-02 18:58:08 +0800

    feat(tune.sh): dumb automation for training

---
+++ tune.sh
@@ -0,0 +1,3 @@
+python3 -m plb.algorithms.solve --algo nn --env_name Move-v1 --seed 0
+# python3 -m plb.algorithms.solve --algo nn --env_name Move-v1 --seed 10
+python3 -m plb.algorithms.solve --algo nn --env_name Move-v1 --seed 20
